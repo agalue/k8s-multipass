1cd902fe9f23bc7a9e86655fd73cf9cfa04eadb6
Alejandro Galue <galue.alejandro@gmail.com>
2023-12-20 10:23:17 -0500

    Simplify Repository. (#4)
    Co-authored-by: Alejandro Galue <agalue@opennms.org>

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 This repository contains a simple script to set up a simple Kubernetes cluster via Kubeadm for learning purposes. It deploys the latest version (v1.29) using Cilium as CNI without Kubeproxy. It will create Ubuntu 22.04 LTS VMs on your machine using Multipass.
 
+This allows you to deploy either a single master or a multi-master deployment.
+
 ## Requirements
 
 Make sure you have [Multipass](https://multipass.run/) installed on your machine.
 
 ## Start the cluster
 
-The following starts a cluster with a single master and 3 workers with 2 CPUs and 16GB of RAM per instance:
+The following starts a cluster with 3 masters behind a proxy and 3 workers with 2 CPUs and 16GB of RAM per instance:
 
 ```bash=
 ./start.sh --workers 3 --cpus 2 --memory 16
@@ -16,23 +18,33 @@ The following starts a cluster with a single master and 3 workers with 2 CPUs an
 
 Default values are:
 
+* 3 master nodes (`--masters`) - use 1 for a single master
 * 2 worker nodes (`--workers`)
 * 2 CPUs per VM (`--cpus`)
 * 8 GB of RAM per VM (`--memory`)
 * 50 GB for Disk per worker VM (`--disk`)
-* All node kinds share the same CPU, Memory, and Disk settings
+* All node kinds share the same CPU and Memory.
 
 The cluster is initialized using [cloud-init](https://cloudinit.readthedocs.io/en/latest/), and all the detailes live inside the [kubernetes.yaml](./kubernetes.yaml) file.
 
 ## Interact with the cluster
 
-Open a session against the master:
+If you choose to have only one master, open a session against the master:
 
 ```bash=
 multipass shell k8smaster
 ```
 
 From there, `kubectl` is already configured for the default user (i.e., `ubuntu`), and the alias `k` can be used. In both cases, bash-completion is enabled.
+
+However, if you choose to have multiple master servers, an additional load balancer was configured, and you can do the following from hour machine (assuming you have `kubectl` installed):
+
+```bash=
+export KUBECONFIG=$(pwd)/kube_config.conf
+kubectl get nodes
+```
+
+> The `kube_config.conf` file was created by the `start.sh` script and should use the LB to access the cluster.
 
 ## Clean up
 
--- start.sh
+++ start.sh
@@ -4,14 +4,29 @@ set -euo pipefail
 trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 type multipass >/dev/null 2>&1 || { echo >&2 "multipass required but it's not installed; aborting."; exit 1; }
 
-# Environment variables
+# Configurable Environment variables
+masters="3"
 workers="2"
 cpus="2"
 memory="8"
 disk="50"
 
+if [[ $# -gt 0 ]] && [[ "$1" == "-h" ]]; then
+  cat <<EOF
+Usage: start.sh [parameters]
+
+Parameters:
+--masters n   Number of masters (default: $masters)
+--workers n   Number of workers (default: $workers)
+--cpus n      Number of CPUs per VM (default: $cpus)
+--memory n    Amount of Memory in GB per VM (default: $memory)
+--disk n      Amount of Disk in GB per worker VM (default: $disk)
+EOF
+  exit
+fi
+
 # Processing parameters
-while [ $# -gt 0 ]; do
+while [[ $# -gt 0 ]]; do
   if [[ $1 == *"--"* ]]; then
     param="${1/--/}"
     declare $param="$2"
@@ -19,35 +34,91 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-# Validation
-if [ ${cpus} -lt 2 ]; then
-  echo "ERROR: A minimum of 2 CPUs per VM is required."
+# Validate parameters
+if [[ ${cpus} < 2 ]]; then
+  echo "ERROR: A minimum of 2 CPUs per VM is required (you specified ${cpus})."
   exit 1
 fi
-if [ ${memory} -lt 2 ]; then
-  echo "ERROR: A minimum of 2GB of RAM per VM is required."
+if [[ ${memory} < 2 ]]; then
+  echo "ERROR: A minimum of 2GB of RAM per VM is required (you specified ${memory})."
   exit 1
 fi
 
-echo "Creating cluster using: workers=${workers}, cpus=${cpus}, memory=${memory}g, disk=${disk}g"
+# Internal Fixed Variables
+proxy_hostname="k8smain"
+master_prefix="k8smaster"
+worker_prefix="k8sworker"
 
-# Temporal variables
-cmd="setup_worker.sh"
+# Infrastructure Verification
+nodes=$(($masters + $workers))
+total_cpus=$(($cpus * $nodes))
+total_memory=$(($memory * $nodes))
+if [[ ${masters} > 1 ]]; then
+  nodes=$(($nodes + 1))
+  total_cpus=$(($total_cpus + 1))
+  total_memory=$(($total_memory + 1))
+fi
+echo "Creating cluster using ${masters} masters, ${workers} workers; each VM with ${cpus} CPUs, ${memory}GB of RAM; workers will have ${disk}GB of Disk."
+echo "This environment requires ${nodes} VMs, using ${total_cpus} CPUs and ${total_memory}GB of RAM from your machine."
+if [[ ${masters} > 1 ]]; then
+  echo "An additional VM with 1 CPU and 1GB of RAM will be started as the Load Balancer."
+fi
 
-# Start and configure Master (do not change vm name)
-echo "Creating Master..."
-multipass launch -c ${cpus} -m ${memory}g -n k8smaster --cloud-init kubernetes.yaml
-multipass exec k8smaster -- sudo kubernetes-setup-master.sh
-multipass transfer k8smaster:/tmp/${cmd} .
+read -p "Do you want to proceed? [y/n]" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  exit
+fi
+echo "Creating Kubernetes cluster..."
 
-# Start and configure Workers (do not change vm name)
-for i in $(seq 1 ${workers}); do
-  worker="k8sworker${i}"
-  echo "Creating Worker ${worker}..."
-  multipass launch -c ${cpus} -m ${memory}g -d ${disk}g -n ${worker} --cloud-init kubernetes.yaml
-  multipass transfer ./${cmd} ${worker}:/tmp/
-  multipass exec ${worker} -- sudo bash /tmp/${cmd}
+# Start VMs for the Master nodes
+for i in $(seq 1 ${masters}); do
+  master="${master_prefix}${i}"
+  echo "Starting Master ${master}..."
+  multipass launch -c ${cpus} -m ${memory}g -n ${master} --cloud-init kubernetes.yaml
 done
-rm ./${cmd}
 
-echo "Done!"
+# Start VMs for the Worker nodes
+for i in $(seq 1 ${workers}); do
+  worker="${worker_prefix}${i}"
+  echo "Starting Worker ${worker}..."
+  multipass launch -c ${cpus} -m ${memory}g -d ${disk}g -n ${worker} --cloud-init kubernetes.yaml
+done
+
+# Configure Control Plane nodes
+if [[ ${masters} > 1 ]]; then
+  echo "Configuring Load Balancer..."
+  multipass launch -c 1 -m 1g -n ${proxy_hostname} --cloud-init load-balancer.yaml
+  multipass exec ${proxy_hostname} -- sudo /etc/haproxy/setup.sh ${masters} ${master_prefix}
+  master="${master_prefix}1"
+  echo "Initializing primary master node ${master}..."
+  multipass exec ${master} -- sudo kubernetes-setup-primary-master.sh ${proxy_hostname}.local
+  multipass transfer ${master}:/tmp/setup_secondary_master.sh .
+  multipass transfer ${master}:/tmp/setup_worker.sh .
+  for i in $(seq 2 ${masters}); do
+    master="${master_prefix}${i}"
+    echo "Initializing secondary master node ${master}..."
+    multipass transfer ./setup_secondary_master.sh ${master}:/tmp/
+    multipass exec ${master} -- sudo bash kubernetes-setup-secondary-master.sh
+  done
+else
+  master="${master_prefix}1"
+  echo "Initializing single master node ${master}..."
+  multipass exec ${master} -- sudo kubernetes-setup-single-master.sh
+  multipass transfer ${master}:/tmp/setup_worker.sh .
+fi
+multipass transfer ${master_prefix}1:/home/ubuntu/.kube/config kube_config.conf
+
+# Configure Worker nodes
+for i in $(seq 1 ${workers}); do
+  worker="${worker_prefix}${i}"
+  echo "Initializing worker node ${worker}..."
+  multipass transfer ./setup_worker.sh ${worker}:/tmp/
+  multipass exec ${worker} -- sudo bash /tmp/setup_worker.sh
+done
+
+# Finalizing
+rm -f ./setup_secondary_master.sh ./setup_worker.sh
+echo 'To access the cluster locally use:'
+echo 'export KUBECONFIG=$(pwd)/kube_config.conf'
+echo 'Done!'
